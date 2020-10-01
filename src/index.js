cafa33af4072d6e44c15cbb62575db0d7ff08e17
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-10-01 10:09:46 +0300

    fix: implement correct functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    } else return Math.min(...array);
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    } else return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    if (array.length === 0) {
+        return 0;
+    } else return array.reduce((sum, current) => sum + current, 0)/array.length;
 }
